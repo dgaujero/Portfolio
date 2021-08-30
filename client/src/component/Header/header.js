4315609e082ded4fc25f2085bf1484f1a8572ea7
dgaujero <deion@aujero.net>
2021-08-30 15:57:50 -0400

    fixing link to homepage

--- client/src/component/Header/header.js
+++ client/src/component/Header/header.js
@@ -77,7 +77,7 @@ function Header(){
             <div className='section3'>
                 <div className='tabs'>
                     <ul>
-                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="Portfolio/">Home</Link></li>
                         <li><Link to='/about'>About</Link></li>
                         <li><Link to='/contact'>Contact</Link></li>
                         <li><Link to='/skills'>Skills</Link></li>
