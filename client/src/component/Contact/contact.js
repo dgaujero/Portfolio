854c5bb151bcc9fb4d98a593256dc3c453d30684
dgaujero <deion@aujero.net>
2021-08-30 13:59:59 -0400

    socials and links to socials added

--- client/src/component/Contact/contact.js
+++ client/src/component/Contact/contact.js
@@ -9,6 +9,13 @@ const Contact = () => {
                 <article>
                     <p>Reach Out On My Socials</p>
                 </article>
+                <div className='socials'>
+                    <p>1. &nbsp; <span>.socials </span>	&#123;</p>
+                    <p className='social'>2. &nbsp; &nbsp; &nbsp; &nbsp; email: <span><a href = "mailto: deion@aujero.net">deion@aujero.com</a></span></p>
+                    <p className='social'>3. &nbsp; &nbsp; &nbsp; &nbsp; github: <span><a target='#' href='https://github.com/dgaujero'>https://github.com/dgaujero</a></span></p>
+                    <p className='social'>4. &nbsp; &nbsp; &nbsp; &nbsp; instagram: <span><a target='#' href='https://instagram.com/deion.aujero'>@deion.aujero</a></span></p>
+                    <p> &nbsp; &nbsp; &nbsp; &#125;</p>
+                </div>
             </div>
 
             <div className='formSection'>
